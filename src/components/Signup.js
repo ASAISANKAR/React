b9624258dcb2b290c6b08c28650a6ba85e541779
saisankar <saisankar3193@gmail.com>
2024-11-05 20:22:03 +0530

    update

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -103,12 +103,7 @@ export default function Signup() {
                   autoComplete="new-password"
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
+
             </Grid>
             <Button
               type="submit"
@@ -120,9 +115,7 @@ export default function Signup() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+               
               </Grid>
             </Grid>
           </Box>
